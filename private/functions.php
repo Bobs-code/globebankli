fb252abf319fdf98778cac26c6b76a5b246e8899
Bobby Griffin <robertgriffintm@gmail.com>
2022-12-13 16:03:14 -0500

    added Http error function

--- private/functions.php
+++ private/functions.php
@@ -14,4 +14,14 @@ function u($string = ""){
 function spCh($string = ""){
    return htmlspecialchars($string);
 }
+
+function error_404(){
+   header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+   exit();
+}
+
+function error_500(){
+   header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+   exit();
+}
 ?>
